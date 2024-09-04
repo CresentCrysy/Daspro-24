82533b153783e1d28411eef45e6a0e4f71c71195
CresentCrysy <raymondevtant@gmail.com>
2024-09-04 16:41:17 +0700

    Second commit

---
+++ W1-Install-JDK/Latihan26.java
@@ -0,0 +1,6 @@
+public class Latihan26 {
+    public static void main (String [] a){
+        System.out.print("Nama saya adalah Raymon Devtant");
+        //System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ W1-Install-JDK/Tugas26.java
@@ -0,0 +1,15 @@
+public class Tugas26 {
+    public static void main(String[] a) {
+        System.out.println("------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi Polinema");
+        System.out.println("------------------------------------------------------");
+        System.out.println("Nama\t: Raymon Devtant");
+        System.out.println("NIM\t: 244107020009");
+        System.out.println("Kelas\t: 1A");
+        System.out.println("No Absen: 26");
+        System.out.println("Prodi\t: Teknik Informatika");
+        System.out.println("Alamat\t: Jalan Anggrek Gang 9B No.1 Situbondo ");
+        System.out.println("No HP\t: 082142100120");
+        System.out.println("Hobi\t: Games");
+    }
+}
